30dc3250a69d594189c41692a5aa50ab12d3c958
qolbudr <qolbu.dzikru30@gmail.com>
2024-01-07 10:47:33 +0700

    change player mode

--- lib/presentation/pages/player.dart
+++ lib/presentation/pages/player.dart
@@ -1,8 +1,9 @@
 import 'package:better_player/better_player.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:netflix/constant.dart';
-import 'package:netflix/data/model/home_model.dart';
+import 'package:netflix/data/model/movie_model.dart';
 
 class Player extends StatelessWidget {
   const Player({super.key, required this.argument});
@@ -11,38 +12,54 @@ class Player extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: BetterPlayer(
-        controller: BetterPlayerController(
-          BetterPlayerConfiguration(
-            fit: BoxFit.contain,
-            autoPlay: true,
-            autoDetectFullscreenDeviceOrientation: true,
-            showPlaceholderUntilPlay: true,
-            subtitlesConfiguration: const BetterPlayerSubtitlesConfiguration(
-              fontSize: 18,
-              backgroundColor: Colors.black,
-            ),
-            controlsConfiguration: BetterPlayerControlsConfiguration(
-              playerTheme: BetterPlayerTheme.material,
-              showControlsOnInitialize: false,
-              overflowModalColor: bgColor,
-              overflowModalTextColor: Colors.white,
-              overflowMenuIconsColor: Colors.white,
-            ),
-          ),
-          betterPlayerDataSource: BetterPlayerDataSource(
-            BetterPlayerDataSourceType.network,
-            argument.url,
-            placeholder: CachedNetworkImage(imageUrl: 'https://image.tmdb.org/t/p/w500/${argument.movie.backdropPath}'),
-            cacheConfiguration: const BetterPlayerCacheConfiguration(useCache: true),
-            subtitles: [
-              BetterPlayerSubtitlesSource(
-                type: BetterPlayerSubtitlesSourceType.memory,
-                content: argument.subtitle,
-                selectedByDefault: true,
+    return PopScope(
+      onPopInvoked: (didPop) {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitDown,
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.landscapeRight,
+          DeviceOrientation.landscapeLeft,
+        ]);
+        Navigator.popUntil(context, (route) => route.settings.name == "/detail");
+      },
+      child: Material(
+        child: BetterPlayer(
+          controller: BetterPlayerController(
+            BetterPlayerConfiguration(
+              deviceOrientationsAfterFullScreen: [
+                DeviceOrientation.landscapeRight,
+                DeviceOrientation.landscapeLeft,
+              ],
+              fit: BoxFit.contain,
+              autoPlay: true,
+              autoDetectFullscreenDeviceOrientation: true,
+              showPlaceholderUntilPlay: true,
+              subtitlesConfiguration: const BetterPlayerSubtitlesConfiguration(
+                fontSize: 18,
+                backgroundColor: Colors.black,
               ),
-            ],
+              controlsConfiguration: BetterPlayerControlsConfiguration(
+                enableFullscreen: false,
+                playerTheme: BetterPlayerTheme.material,
+                showControlsOnInitialize: false,
+                overflowModalColor: bgColor,
+                overflowModalTextColor: Colors.white,
+                overflowMenuIconsColor: Colors.white,
+              ),
+            ),
+            betterPlayerDataSource: BetterPlayerDataSource(
+              BetterPlayerDataSourceType.network,
+              argument.url,
+              placeholder: CachedNetworkImage(imageUrl: 'https://image.tmdb.org/t/p/w500/${argument.movie.tmdb?.backdropPath}'),
+              cacheConfiguration: const BetterPlayerCacheConfiguration(useCache: true),
+              subtitles: [
+                BetterPlayerSubtitlesSource(
+                  type: BetterPlayerSubtitlesSourceType.memory,
+                  content: argument.subtitle,
+                  selectedByDefault: true,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -52,7 +69,7 @@ class Player extends StatelessWidget {
 
 class PlayerArgument {
   final String url;
-  final String subtitle;
+  final String? subtitle;
   final Movie movie;
   PlayerArgument({required this.url, required this.subtitle, required this.movie});
 }
